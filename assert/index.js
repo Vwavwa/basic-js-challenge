b80de6cb540d92dbcbb2bdc683db357f65f0a050
Grayfield.Miracle <grayfieldmm@gmail.com>
2019-12-10 22:29:48 +0700

    add assert lib

---
+++ assert/index.js
@@ -0,0 +1,29 @@
+const fail = (actual, expected)  => {
+  // throw new assert.AssertionError({
+  //   message: message,
+  //   actual: actual,
+  //   expected: expected,
+  //   operator: operator,
+  //   stackStartFunction: stackStartFunction
+  // });
+  console.error(
+    `[\u2718] ${actual}`,
+    `\n > should be: ${expected}`,
+  );
+}
+
+const success = (actual) => {
+  console.log(`[\u2714] ${actual}`);
+}
+
+const strictEqual = (actual, expected, message) => {
+  if (actual !== expected) {
+    fail(actual, expected)
+  } else {
+    success(actual);
+  }
+}
+
+module.exports = {
+  strictEqual
+}
