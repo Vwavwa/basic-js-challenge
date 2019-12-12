3426f0f81e46e9b78419e5c9f684273422a46036
Grayfield.Miracle <grayfieldmm@gmail.com>
2019-12-12 14:45:02 +0700

    adjust assert libs to print the actual and expected value and adjust 2nd/002.test.js

--- assert/index.js
+++ assert/index.js
@@ -1,3 +1,10 @@
+const consoleFontColorSuccess = '\x1b[34m';
+const consoleFontColorFailed = '\x1b[31m';
+const consoleFontColorNormal = '\x1b[0m';
+
+const successMark = '\u2714';
+const failedMark = '\u2718';
+
 const fail = (actual, expected)  => {
   // throw new assert.AssertionError({
   //   message: message,
@@ -7,20 +14,22 @@ const fail = (actual, expected)  => {
   //   stackStartFunction: stackStartFunction
   // });
   console.error(
-    `[\u2718] ${actual}`,
-    `\n > should be: ${expected}`,
+    consoleFontColorFailed, `[${failedMark}]`,
+    consoleFontColorNormal, { actual, expected }
   );
 }
 
-const success = (actual) => {
-  console.log(`[\u2714] ${actual}`);
+const success = (actual, expected) => {
+  console.log(
+    consoleFontColorSuccess, `[${successMark}]`,
+    consoleFontColorNormal, { actual, expected });
 }
 
 const strictEqual = (actual, expected, message) => {
   if (actual !== expected) {
     fail(actual, expected);
   } else {
-    success(actual);
+    success(actual, expected);
   }
 }
 
@@ -28,7 +37,7 @@ const deepEqual = (actual, expected) => {
   if (JSON.stringify(actual) !== JSON.stringify(expected)) {
     fail(actual, expected);
   } else {
-    success(actual);
+    success(actual, expected);
   }
 }
 
