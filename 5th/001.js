ce904d282a545313336be3c3a666beb92c43f9ec
mariosupit <armandosupit@gmail.com>
2020-01-15 03:35:32 +0700

    finished 5th/001

--- 5th/001.js
+++ 5th/001.js
@@ -9,23 +9,47 @@ menghapus dan jumlah data yang akan dihapus
 
 Contoh:
 Input:
-  array: [ 666, 666, 333, 10, 5, 6]
+  array: [ 777, 666, 333, 10, 5, 6]
   index: 1
   count: 2
 
 Output:
-  [ 666, 10, 5, 6 ]
+  [ 777, 10, 5, 6 ]
 
 RULES:
   - WAJIB MENYERTAKAN ALGORITMA / PSEUDOCODE
   - Dilarang menggunakan built-in function .map, .filter, .reduce!
 */
- 
-function arrayRemover (array, index, count) {
+
+function arrayRemover(array, index, count) {
   // your code here
+  // hapus jumlah data pakai array.slice(parameter kedua, parameter ketiga)
+  // array.splice(index, count)
 
+  // buat variable baru untuk store hasil dengan nama result
+  // jika count = panjang array
+  // return result
+  // looping untuk array
+  // jika i < index || i > count
+  // push array ke index tersebut
+    // jika i = index
+    // push array ke index + count
+
+  let result = []
+  if(count == array.length) {
+    return result
+  }
+  for(let i = 0; i < array.length; i++) {
+    if(i < index || i > count ) {
+      result.push(array[i])
+    }else if(i == index){
+      result.push(array[i+count])
+    }
+  }
+
+  
+  return result
 }
-
-console.log(arrayRemover([ 1, 2, 3] , 0 , 2 )); // [3]
- console.log(arrayRemover([ 666, 666, 333, 10, 5, 6] , 1 , 2 )); // [ 666, 10, 5, 6 ]
-console.log(arrayRemover([ 1, 2, 3], 0, 3)) // []
+console.log(arrayRemover([1, 2, 3], 0, 2)); // [3]
+console.log(arrayRemover([777, 666, 333, 10, 5, 6], 1, 2)); // [ 777, 10, 5, 6 ]
+console.log(arrayRemover([1, 2, 3], 0, 3)) // []
